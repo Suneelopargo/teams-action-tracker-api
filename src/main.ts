53a134b56dcddd3e4782bb835f5fa565f5842975
SuneelOpargo <suneel.kumar@solventek.com>
2026-06-23 14:25:29 +0530

    Added the swagger code

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,47 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import helmet from 'helmet';
+import compression from 'compression';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.use(helmet());
+  app.use(compression());
+
+  app.setGlobalPrefix('api');
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  const config = new DocumentBuilder()
+    .setTitle('Teams Action Tracker API')
+    .setDescription('Teams Meeting Action Tracker')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api/docs', app, document);
+
+  await app.listen(3000);
+
+  console.log(
+    `Application running at: http://localhost:3000/api`,
+  );
+
+  console.log(
+    `Swagger running at: http://localhost:3000/api/docs`,
+  );
 }
+
 bootstrap();
--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,12 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
 import { UserService } from './user.service';
 
+@ApiTags('Users')
 @Controller('users')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Get()
-  async getUsers() {
+  getUsers() {
     return this.userService.getAllUsers();
   }
 }
